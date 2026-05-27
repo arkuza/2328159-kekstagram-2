1f6858bcb0df25fadb0186dab68c8a99a7f8fece
arkuza <147090778+arkuza@users.noreply.github.com>
2026-05-27 20:32:05 +0300

    Вносит изменения в файл  main.js  исправляя замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 /*  Список констант и массивы данных*/
 const PHOTO_ID_MAX = 25;
 const PHOTO_ID_MIN = 1;
-const URL_MIN = 1;
-const URL_MAX = 25;
+const IMAGE_PATH_MIN = 1;
+const IMAGE_PATH_MAX = 25;
 const LIKES_MAX = 200;
 const LIKES_MIN = 15;
 const COMMENT_ID_MIN = 1;
-const COMMENT_ID_MAX = 1000;
+const COMMENT_ID_MAX = 10000;
 const AVATAR_MIN = 1;
 const AVATAR_MAX = 6;
 const MESSAGE_AMOUNT_MIN = 1;
 const MESSAGE_AMOUNT_MAX = 2;
 
 
-const URL = [];
-for(let i = URL_MIN; i < URL_MAX; i++) {
+const IMAGE_PATHS = [];
+for(let i = IMAGE_PATH_MIN; i < IMAGE_PATH_MAX; i++) {
   const RESULT = `photos/${i}.jpg`;
-  URL.push(RESULT);
+  IMAGE_PATHS.push(RESULT);
 }
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'Отдыхайте так, чтобы вы забывали брать телефон в руки',
   'Я не знаю, куда иду, но я уже в пути' ,
   'Говорят «делай, что любишь, и деньги сами придут к тебе». Только что заказала доставку. Жду',
@@ -53,18 +53,13 @@ for(let i = LIKES_MIN; i < LIKES_MAX; i++){
   LIKES.push(i);
 }
 
-const COMMENT_ID = [];
-for(let i = COMMENT_ID_MIN; i <= COMMENT_ID_MAX; i++) {
-  COMMENT_ID.push(i);
-}
-
-const AVATAR = [];
+const AVATARS = [];
 for(let i = AVATAR_MIN; i <= AVATAR_MAX; i++) {
   const TOTAL = `img/avatar-${i}.svg`;
-  AVATAR.push (TOTAL);
+  AVATARS.push (TOTAL);
 }
 
-const MESSAGE = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -73,7 +68,7 @@ const MESSAGE = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const NAME = [
+const NAMES = [
   'Викория',
   'Николай',
   'Энрике',
@@ -108,6 +103,7 @@ const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
+
   return Math.floor(result);
 };
 
@@ -119,8 +115,8 @@ function createRandomIntegerFromRangeGenerator (min, max) {
   return function () {
     let currentValue = getRandomInteger(min, max);
     if (previousValues.length >= (max - min + 1)) {
-      // console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
-      // return null;
+
+      return null;
     }
     while (previousValues.includes(currentValue)) {
       currentValue = getRandomInteger(min, max);
@@ -130,23 +126,25 @@ function createRandomIntegerFromRangeGenerator (min, max) {
   };
 }
 
-const commentIdGenerator = createRandomIntegerFromRangeGenerator(1, 10000);
+const commentIdGenerator = createRandomIntegerFromRangeGenerator(COMMENT_ID_MIN, COMMENT_ID_MAX);
 
 /* Функция для генерации данных комментария*/
 const createComment = () => {
   const messagesAmount = getRandomInteger(MESSAGE_AMOUNT_MIN, MESSAGE_AMOUNT_MAX);
-  const messageIndexGenerator = createRandomIntegerFromRangeGenerator(0, MESSAGE.length - 1);
+  const messageIndexGenerator = createRandomIntegerFromRangeGenerator(0, MESSAGES.length - 1);
   let resultMessage = '';
+
   for(let i = 0; i < messagesAmount; i++) {
-    resultMessage += `${MESSAGE[messageIndexGenerator()]} `;
+    resultMessage += `${MESSAGES[messageIndexGenerator()]} `;
   }
+
   resultMessage = resultMessage.slice(0, -1);
 
   return{
     id : commentIdGenerator(),
-    avatar : getRandomArrayElement(AVATAR),
+    avatar : getRandomArrayElement(AVATARS),
     message: resultMessage,
-    name: getRandomArrayElement(NAME),
+    name: getRandomArrayElement(NAMES),
   };
 
 };
@@ -155,22 +153,19 @@ const createComment = () => {
 
 const photoIdGenerator = createRandomIntegerFromRangeGenerator(PHOTO_ID_MIN, PHOTO_ID_MAX);
 
-const photoUrlGenerator = createRandomIntegerFromRangeGenerator(URL_MIN, URL_MAX);
-
+const photoUrlGenerator = createRandomIntegerFromRangeGenerator(IMAGE_PATH_MIN, IMAGE_PATH_MAX);
 
 const createFotoDescribing = () => {
   const COMMENTS_AMOUNT = getRandomInteger(0, 30);
   const COMMENTS = Array.from({length : COMMENTS_AMOUNT}, createComment);
+
   return {
     id : photoIdGenerator(),
     url : `photos/${photoUrlGenerator()}.jpg`,
-    description :getRandomArrayElement(DESCRIPTION),
+    description :getRandomArrayElement(DESCRIPTIONS),
     likes : getRandomInteger(LIKES_MIN, LIKES_MAX),
     comments:  COMMENTS,
   };
 };
 
 const photosDescriptions = Array.from({length : 25}, createFotoDescribing);
-console.log(photosDescriptions);
-
-
